b20fc799d001d14f6d3da6703944abef6f51bd80
NurmuhammadNikita <888legolas777@gmail.com>
2023-11-02 18:24:14 +0500

    Code rub

--- src/UMS.Domain/Entities/Student.cs
+++ src/UMS.Domain/Entities/Student.cs
@@ -1,16 +1,16 @@
 ﻿namespace UMS.Domain.Entities
 {
-    public class Student:Auditable
+    public class Student : Auditable
     {
         public int Id { get; set; }
         public int PersonalDataId { get; set; }
         public int Course { get; set; }
         public int SpecialtyEduFormId { get; set; }
-        public int  IsActive {  get; set; }
+        public int IsActive { get; set; }
         public string EduType { get; set; }
         public string GroupNumber { get; set; }
         public int SubjectId { get; set; }
-        public DateTime CreatedAt {  get; set; }
-        public DateTime UpdatedAt { get; set;}
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
