eef8ad8a93538b06a7307aead46a9e9063137245
NurmuhammadNikita <888legolas777@gmail.com>
2023-11-02 18:22:20 +0500

    update edu form model

--- src/UMS.Domain/Entities/EduForm.cs
+++ src/UMS.Domain/Entities/EduForm.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UMS.Domain.Entities
+﻿namespace UMS.Domain.Entities
 {
-    public class EduForm:Auditable
+    public class EduForm : Auditable
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set;}
+        public DateTime UpdatedAt { get; set; }
     }
 }
