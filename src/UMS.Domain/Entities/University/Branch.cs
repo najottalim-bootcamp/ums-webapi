01590b7c54f8d10c984499e856d6a612c517243c
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-02 22:29:36 +0500

    University folder added

---
+++ src/UMS.Domain/Entities/University/Branch.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UMS.Domain.Entities.University
+{
+    public class Branch : BaseEntity
+    {
+        public string Name { get; set; }
+        public int UniversityId { get; set; }
+        public string Address { get; set; }
+        public int CityID { get; set; }
+        public string PostCode { get; set; }
+    }
+}
+
