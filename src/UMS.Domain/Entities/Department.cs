bf2de2772f9644eeed2429a34090b407df2e413e
Jurayevkh <jurayevkh@gmail.com>
2023-11-02 18:28:13 +0500

    Added Rustambek's own table

---
+++ src/UMS.Domain/Entities/Department.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UMS.Domain.Entities
+{
+	public class Department:BaseEntity
+	{
+		public int FacultyID { get; set; }
+		public string Name { get; set; }
+	}
+}
+
---
+++ src/UMS.Domain/Entities/University.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UMS.Domain.Entities
+{
+	public class University:BaseEntity
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Image_path { get; set; }
+	}
+}
+
