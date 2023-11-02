8254b7417b6f2219a5d9e23d5c0a361015ca3f55
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-02 22:29:13 +0500

    Teachers folder added

---
+++ src/UMS.Domain/Entities/Teachers/Teacher.cs
@@ -0,0 +1,10 @@
+﻿namespace UMS.Domain.Entities.Teacher
+{
+    public class Teacher : Auditable
+    {
+        public int DepartmentId { get; set; }
+        public int PersonalDataId { get; set; }
+        public int AcadPositionId { get; set; }
+        public int ScienDegreeId { get; set; }
+    }
+}
