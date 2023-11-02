3462d8120668ec8ba7eaa6314d5513dda8df5874
NurmuhammadNikita <888legolas777@gmail.com>
2023-11-02 16:32:52 +0500

    Nurmuhammad added his own tables

---
+++ src/UMS.Domain/Entities/Specialty.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Domain.Entities
+{
+    public class Specialty:BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ src/UMS.Domain/Entities/SpecialtyEduForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMS.Domain.Entities
+{
+    public class SpecialtyEduForm:BaseEntity
+    {
+        public int Id { get; set; }
+        public int EduFormId { get; set; }
+        public int SpecialtyId { get; set; }
+
+    }
+}
