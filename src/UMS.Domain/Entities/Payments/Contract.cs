537866d02065e81246d2c77824db12f0c8c579a3
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-02 22:28:38 +0500

    Payments foldre added

---
+++ src/UMS.Domain/Entities/Payments/Contract.cs
@@ -0,0 +1,9 @@
+﻿namespace UMS.Domain.Entities.Payments
+{
+    public class Contract : Auditable
+    {
+        public int FacultId { get; set; }
+        public int StudentId { get; set; }
+        public float Price { get; set; }
+    }
+}
