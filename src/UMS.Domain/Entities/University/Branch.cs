7678683173163047f7c0c69548866592f90a7507
Nurmuhammad Davletov <119412196+devnurmuhammad@users.noreply.github.com>
2023-11-03 11:27:54 +0500

    Update Branch.cs
    fixed

--- src/UMS.Domain/Entities/University/Branch.cs
+++ src/UMS.Domain/Entities/University/Branch.cs
@@ -1,13 +1,11 @@
-﻿using System;
-
 namespace UMS.Domain.Entities.University
 {
     public class Branch : BaseEntity
     {
         public string Name { get; set; }
-        public int UniversityId { get; set; }
+        public long UniversityId { get; set; }
         public string Address { get; set; }
-        public int CityID { get; set; }
+        public long CityID { get; set; }
         public string PostCode { get; set; }
     }
 }
