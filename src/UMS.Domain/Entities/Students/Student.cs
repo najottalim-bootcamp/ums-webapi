080818ba611f6a4e465e7c9b5d8abee9615acb43
Nurmuhammad Davletov <119412196+devnurmuhammad@users.noreply.github.com>
2023-11-03 11:26:16 +0500

    Update Student.cs
    fixed

--- src/UMS.Domain/Entities/Students/Student.cs
+++ src/UMS.Domain/Entities/Students/Student.cs
@@ -2,12 +2,12 @@
 {
     public class Student : Auditable
     {
-        public int PersonalDataId { get; set; }
+        public long PersonalDataId { get; set; }
         public int Course { get; set; }
-        public int SpecialtyEduFormId { get; set; }
-        public int IsActive { get; set; }
+        public long SpecialtyEduFormId { get; set; }
+        public bool IsActive { get; set; }
         public string EduType { get; set; }
         public string GroupNumber { get; set; }
-        public int SubjectId { get; set; }
+        public long SubjectId { get; set; }
     }
 }
