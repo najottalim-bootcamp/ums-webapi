e319d4b1e6f928edd7736d5ab68e8300250d425e
Nurmuhammad Davletov <119412196+devnurmuhammad@users.noreply.github.com>
2023-11-03 11:28:29 +0500

    Update PersonalData.cs
    fixed

--- src/UMS.Domain/Entities/User/PersonalData.cs
+++ src/UMS.Domain/Entities/User/PersonalData.cs
@@ -7,8 +7,8 @@ namespace UMS.Domain.Entities.People
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
-        public int CityId { get; set; }
-        public int CountryId { get; set; }
+        public long CityId { get; set; }
+        public long CountryId { get; set; }
         public string Email { get; set; }
         public Gender Gender { get; set; }
         public string PhoneNumber { get; set; }
