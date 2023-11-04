545350be7d7760e87a626cc93f2a8e9cead38891
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-04 10:50:57 +0500

    updated country interface

--- src/UMS.DataAccess/Repositories/Countries/ICountryRepository.cs
+++ src/UMS.DataAccess/Repositories/Countries/ICountryRepository.cs
@@ -1,4 +1,4 @@
-﻿namespace UMS.DataAccess.Repositories.CountryPositions
+﻿namespace UMS.DataAccess.Repositories.Countries
 {
     internal interface ICountryRepository:IBaseRepository<Country,CountryDto>
     {
