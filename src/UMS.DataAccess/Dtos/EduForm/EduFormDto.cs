3b3a06960500cc5b8407310a12bdeba8ad99b597
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-04 18:28:25 +0500

    EduFormDTO added

--- src/UMS.DataAccess/Dtos/EduForm/EduFormDto.cs
+++ src/UMS.DataAccess/Dtos/EduForm/EduFormDto.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static UMS.Domain.Enums.EduFormEnum;
+﻿using static UMS.Domain.Enums.EduFormEnum;
 
 namespace UMS.DataAccess.Dtos.EduForm
 {
