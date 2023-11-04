387d2380e607487dee3dbc3657dd7045fe33b312
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-11-04 18:28:11 +0500

    SpecialtyEduFormDTO added

---
+++ src/UMS.DataAccess/Dtos/Education/SpecialtyEduFormDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace UMS.DataAccess.Dtos.Education
+{
+    public class SpecialtyEduFormDTO
+    {
+        public long EduFormId { get; set; }
+        public long SpecialtyId { get; set; }
+    }
+}
