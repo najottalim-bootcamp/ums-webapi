a991adbac92bbc1abd0de9c6227034fcfa27efc8
user <user@WIN-F7NIMF7A3VO>
2023-11-04 18:42:00 +0500

    Added Repositories and Dtos by Akbarkhon (for City,Contract,Student,Subject)

--- src/UMS.DataAccess/Dtos/Countries/CountryDto.cs
+++ src/UMS.DataAccess/Dtos/Countries/CountryDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace UMS.DataAccess.Dtos.Countries
+﻿namespace UMS.DataAccess.Dtos.Countries
 {
     public class CountryDto
     {
