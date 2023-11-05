eefb2e89ec0cb42d7b311bb708db180f614326ec
user <user@WIN-F7NIMF7A3VO>
2023-11-06 02:17:25 +0500

    Added SpecialtyEduFormService

---
+++ src/UMS.Service/SpecialtyEduForms/ISpecialtyEduFormService.cs
@@ -0,0 +1,12 @@
+﻿namespace UMS.Service.SpecialtyEduForms
+{
+    public interface ISpecialtyEduFormService
+    {
+        public Task<long> CountAsync();
+        public ValueTask<bool> CreateAsync(SpecialtyEduFormDTO specialtyEduFormDTO);
+        public ValueTask<bool> UpdateAsync(long id, SpecialtyEduFormDTO specialtyEduFormDTO);
+        public ValueTask<bool> DeleteAsync(long id);
+        public ValueTask<SpecialtyEduForm> GetByIdAsync(long id);
+        public ValueTask<IList<SpecialtyEduForm>> GetAllAsync();
+    }
+}
