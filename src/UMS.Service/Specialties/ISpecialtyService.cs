f972e0b8f1a1a28c05b35b62d8c1542eb0bb36e3
Jurayevkh <jurayevkh@gmail.com>
2023-11-05 22:38:56 +0500

    Fixed Mistakes

--- src/UMS.Service/Specialties/ISpecialtyService.cs
+++ src/UMS.Service/Specialties/ISpecialtyService.cs
@@ -1,11 +1,13 @@
-﻿namespace UMS.Service.Specialties
+﻿using UMS.DataAccess.Dtos.Education;
+
+namespace UMS.Service.Specialties
 {
 	public interface ISpecialtyService
 	{
-        public ValueTask<bool> CreateAsync(SubjectDto contractDto);
+        public ValueTask<bool> CreateAsync(SpecialtyDTO specialtyDto);
         public ValueTask<bool> DeleteAsync(long id);
-        public ValueTask<Subject> GetByIdAsync(long id);
-        public ValueTask<IList<Subject>> GetAllAsync();
+        public ValueTask<Specialty> GetByIdAsync(long id);
+        public ValueTask<IList<Specialty>> GetAllAsync();
     }
 }
 
