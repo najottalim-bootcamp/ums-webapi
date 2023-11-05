bf9d0fe0b4a7ad747c325834d60a5be000ae8fe2
Jurayevkh <jurayevkh@gmail.com>
2023-11-05 20:54:38 +0500

    Added Interface of Contract Service

---
+++ src/UMS.Service/Contracts/IContractService.cs
@@ -0,0 +1,15 @@
+﻿using UMS.DataAccess.Dtos.Contracts;
+using UMS.Domain.Entities.Payments;
+
+namespace UMS.Service.Contracts
+{
+	public interface IContractService
+	{
+		public ValueTask<bool> CreateAsync(ContractDto contractDto);
+		public ValueTask<bool> UpdateAsync(long id,ContractDto contractDto);
+		public ValueTask<bool> DeleteAsync(long id);
+		public ValueTask<Contract> GetByIdAsync(long id);
+		public ValueTask<IList<Contract>> GetAllAsync();
+	}
+}
+
