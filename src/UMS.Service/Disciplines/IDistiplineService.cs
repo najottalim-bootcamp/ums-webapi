838a26edb91b35de1a02ef1fd6ab24619d974765
user <user@WIN-F7NIMF7A3VO>
2023-11-06 01:51:31 +0500

    Added Discipline Service

---
+++ src/UMS.Service/Disciplines/IDistiplineService.cs
@@ -0,0 +1,14 @@
+﻿using UMS.DataAccess.Dtos.Discipline;
+
+namespace UMS.Service.Disciplines
+{
+    public interface IDistiplineService
+    {
+        public Task<long> CountAsync();
+        public ValueTask<bool> CreateAsync(DisciplineDto disciplineDto);
+        public ValueTask<bool> UpdateAsync(long id, DisciplineDto disciplineDto);
+        public ValueTask<bool> DeleteAsync(long id);
+        public ValueTask<Discipline> GetByIdAsync(long id);
+        public ValueTask<IList<Discipline>> GetAllAsync();
+    }
+}
