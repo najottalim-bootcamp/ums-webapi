06756fb182d80a32e8d8a81ddfcc746ea144e512
Jurayevkh <jurayevkh@gmail.com>
2023-11-05 22:46:20 +0500

    Added new namespaces and removed useless

--- src/UMS.Service/GlobalUsings.cs
+++ src/UMS.Service/GlobalUsings.cs
@@ -2,12 +2,10 @@
 global using UMS.DataAccess.Repositories.Subjects;
 global using UMS.Domain.Entities.EduModels;
 global using UMS.Domain.Exceptions.Education;
-global using UMS.DataAccess.Dtos.Subjects;
-global using UMS.Domain.Entities.EduModels;
 global using UMS.DataAccess.Dtos.Contracts;
 global using UMS.DataAccess.Repositories.Contracts;
 global using UMS.Domain.Entities.Payments;
 global using UMS.Domain.Exceptions.Payments;
-using UMS.DataAccess.Dtos.Contracts;
-using UMS.Domain.Entities.Payments;
-
+global using UMS.DataAccess.Dtos.Education;
+global using UMS.DataAccess.Repositories.Specialties;
+global using UMS.DataAccess.Dtos.Education;
