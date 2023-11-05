964e0696852625c4a60a1510b39ca40d288e9569
Jurayevkh <jurayevkh@gmail.com>
2023-11-05 22:19:25 +0500

    Added Interface of Subject Service

---
+++ src/UMS.Service/Subjects/ISubjectService.cs
@@ -0,0 +1,15 @@
+﻿using UMS.DataAccess.Dtos.Subjects;
+using UMS.Domain.Entities.EduModels;
+
+namespace UMS.Service.Subjects
+{
+	public interface ISubjectService
+	{
+        public ValueTask<bool> CreateAsync(SubjectDto contractDto);
+        public ValueTask<bool> UpdateAsync(long id, SubjectDto contractDto);
+        public ValueTask<bool> DeleteAsync(long id);
+        public ValueTask<Subject> GetByIdAsync(long id);
+        public ValueTask<IList<Subject>> GetAllAsync();
+    }
+}
+
