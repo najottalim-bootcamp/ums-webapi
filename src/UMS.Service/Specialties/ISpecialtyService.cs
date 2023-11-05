e6ea8b4322144216033f690eab0ccd3761a9a73e
Jurayevkh <jurayevkh@gmail.com>
2023-11-05 22:30:27 +0500

    Created Interface of Specialty Service

---
+++ src/UMS.Service/Specialties/ISpecialtyService.cs
@@ -0,0 +1,11 @@
+﻿namespace UMS.Service.Specialties
+{
+	public interface ISpecialtyService
+	{
+        public ValueTask<bool> CreateAsync(SubjectDto contractDto);
+        public ValueTask<bool> DeleteAsync(long id);
+        public ValueTask<Subject> GetByIdAsync(long id);
+        public ValueTask<IList<Subject>> GetAllAsync();
+    }
+}
+
