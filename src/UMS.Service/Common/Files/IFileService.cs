b72ffe55b293c8658c894e241346b97f5f6254c2
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-06 19:47:32 +0500

    created file service interface

---
+++ src/UMS.Service/Common/Files/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace UMS.Service.Common.Files;
+
+public interface IFileService
+{
+    public Task<string> UploadImageAsync(IFormFile file);
+    public Task<bool> DeleteImageAsync(string file);
+    public Task<string> UploadAvatarAsync(IFormFile file);
+    public Task<bool> DeleteAvatarAsync(string file);
+}
