d591c978d992a5029c7567932286add67fed15bb
Jurayevkh <jurayevkh@gmail.com>
2023-11-07 03:50:38 +0500

    Created Specialty Controller

---
+++ src/UMS.API/Controller/SpecialtyController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UMS.Service.Dtos.Education;
+using UMS.Service.Specialties;
+
+namespace UMS.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialtyController : ControllerBase
+    {
+        private readonly ISpecialtyService _specialtyService;
+
+        public SpecialtyController(ISpecialtyService speciltyService)
+        {
+            _specialtyService = speciltyService;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateAsync([FromForm] SpecialtyDTO specialtyDTO)
+        {
+            bool result = await _specialtyService.CreateAsync(specialtyDTO);
+
+            return Ok(result);
+        }
+
+        [HttpGet("GetAll")]
+        public async ValueTask<IActionResult> GetAllAsync()
+        {
+            var specialties = await _specialtyService.GetAllAsync();
+            return Ok(specialties);
+        }
+
+        [HttpGet("Get")]
+        public async ValueTask<IActionResult> GetByIdAsync(long id)
+        {
+            var specialty = await _specialtyService.GetByIdAsync(id);
+
+            return Ok(specialty);
+        }
+
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteAsync(long id)
+        {
+            bool result = await _specialtyService.DeleteAsync(id);
+
+            return Ok(result);
+        }
+
+
+    }
+}
+
