c9276f56d59a4afa45924b67da93a2cc8dca323d
Jurayevkh <jurayevkh@gmail.com>
2023-11-07 03:59:44 +0500

    Deleted useless namespaces for comfortabilty

--- src/UMS.API/Controller/ContractController.cs
+++ src/UMS.API/Controller/ContractController.cs
@@ -1,10 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using UMS.Service.Contracts;
-using UMS.Service.Dtos.Contracts;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace UMS.API.Controller
+﻿namespace UMS.API.Controller
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
