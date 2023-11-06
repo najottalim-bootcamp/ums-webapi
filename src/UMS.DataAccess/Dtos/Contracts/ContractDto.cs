a4e5bbfbdc66406b39f6ffcca3b075a0ced19330
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-07 01:00:45 +0500

    updated dto namespace

--- src/UMS.DataAccess/Dtos/Contracts/ContractDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UMS.DataAccess.Dtos.Contracts
-{
-    public class ContractDto
-    {
-        public long FacultId { get; set; }
-        public long StudentId { get; set; }
-        public float Price { get; set; }
-    }
-}
