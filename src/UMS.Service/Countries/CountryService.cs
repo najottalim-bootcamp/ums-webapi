a46d3456b8a721524cfb5734e6dc4733763904f7
NurmuhammadNikita <888legolas777@gmail.com>
2023-11-06 11:43:47 +0500

    Country Service Added

--- src/UMS.Service/Countries/CountryService.cs
+++ src/UMS.Service/Countries/CountryService.cs
@@ -1,40 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UMS.DataAccess.Dtos.Contracts;
 using UMS.DataAccess.Dtos.Countries;
-using UMS.Domain.Entities.EduModels;
+using UMS.DataAccess.Repositories.Countries;
+using UMS.Domain.Entities.Locations;
+using UMS.Domain.Exceptions.Locations;
 
 namespace UMS.Service.Countries
 {
     public class CountryService : ICountryService
     {
+        private readonly ICountryRepository _countryRepository;
 
-
-        public CountryService()
+        public CountryService(ICountryRepository countryRepository)
         {
-            
+            _countryRepository = countryRepository;
         }
 
-        public Task<bool> CreateAsync(CountryDto dto)
+        /// <summary> Assinxron qo'shish
+        /// 
+        /// </summary>
+        /// <param name="kiruvchi parametrlar"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+
+        public async Task<bool> CreateAsync(CountryDto dto)
         {
-            throw new NotImplementedException();
+            Country country = new Country()
+            {
+                Name = dto.Name,
+                CreatedAt = DateTime.Now,
+            };
+
+
+            int result = await _countryRepository.CreateAsync(country);
+
+            return result != 0;
         }
 
-        public Task<IList<Faculty>> GetAllAsync()
+        /// <summary>
+        /// Assinxron jadvalni o'qish
+        /// </summary>
+        /// <returns></returns>
+
+        public async Task<IList<Country>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            IList<Country> countries = await _countryRepository.GetAllAsync();
+            return countries;
         }
 
-        public Task<IList<Faculty>> GetById(long id)
+        /// <summary>
+        /// Assinxron obyektni o'qish
+        /// </summary>
+        /// <param name="id"></param> shu Id bo'yicha
+        /// <returns>Country turida obyekt qaytadi</returns>
+        /// <exception cref="CountryNotFoundException"></exception>
+
+        public async Task<Country> GetByIdAsync(long id)
         {
-            throw new NotImplementedException();
+            Country country = await _countryRepository.GetByIdAsync(id);
+            if (country is null) throw new CountryNotFoundException();
+
+            return country;
         }
 
-        public Task<bool> UpdateAsync(long conId, CountryDto dto)
+        /// <summary>
+        /// Assinxron o'zgartirish
+        /// </summary>
+        /// <param name="conId"></param> shu id ga teng obyektni 
+        /// <param name="dto"></param> shu obyektdagi qiymatlarga almashtiradi
+        /// <returns>almashtirilsa 1 aks holda 0 qaytariladi</returns>
+        /// <exception cref="CountryNotFoundException"></exception>
+        public async Task<bool> UpdateAsync(long conId, CountryDto dto)
         {
-            throw new NotImplementedException();
+            Country country = await _countryRepository.GetByIdAsync(conId);
+            if (country is null) throw new CountryNotFoundException();
+
+            country.UpdatedAt = DateTime.Now;
+            country.Name = dto.Name;
+
+
+            int result = await _countryRepository.UpdateAsync(conId,country);
+
+            return result > 0;
         }
+
+       
     }
 }
