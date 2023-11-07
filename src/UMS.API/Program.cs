8049da955b69c73a7ced415b268fdc12b7de8ee4
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-07 17:33:29 +0500

    updated program class

--- src/UMS.API/Program.cs
+++ src/UMS.API/Program.cs
@@ -1,14 +1,18 @@
 using UMS.DataAccess.Repositories.Branchs;
 using UMS.DataAccess.Repositories.Cities;
 using UMS.DataAccess.Repositories.Countries;
+using UMS.DataAccess.Repositories.Departments;
+using UMS.DataAccess.Repositories.Disciplines;
+using UMS.DataAccess.Repositories.Faculties;
 using UMS.DataAccess.Repositories.PersonalDatas;
+using UMS.DataAccess.Repositories.SpecialtyEduForms;
 using UMS.DataAccess.Repositories.Students;
-using UMS.Service.Branches;
-using UMS.Service.Cities;
 using UMS.Service.Common.Files;
 using UMS.Service.Common.Paginations;
-using UMS.Service.Countries;
-using UMS.Service.Services.Students;
+using UMS.Service.Departments;
+using UMS.Service.Disciplines;
+using UMS.Service.Services.Faculties;
+using UMS.Service.SpecialtyEduForms;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,23 +22,36 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 builder.Services.AddScoped<IStudentService, StudentService>();
 builder.Services.AddScoped<IPersonalDataRepository, PersonalDataRepository>();
 builder.Services.AddScoped<IFileService, FileService>();
 builder.Services.AddScoped<IPagination, Pagination>();
 
-builder.Services.AddScoped<ICityRepository , CityRepository>();
-builder.Services.AddScoped<ICityService , CityService>();
+builder.Services.AddScoped<ICityRepository, CityRepository>();
+builder.Services.AddScoped<ICityService, CityService>();
 
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<ICountryService, CountryService>();
 
-builder.Services.AddScoped<ICountryRepository , CountryRepository>();
-builder.Services.AddScoped<ICountryService , CountryService>();
+builder.Services.AddScoped<IBranchRepository, BranchRepository>();
+builder.Services.AddScoped<IBranchService, BranchService>();
 
+builder.Services.AddScoped<ISpecialtyEduFormRepository, SpecialtyEduFormRepository>();
+builder.Services.AddScoped<ISpecialtyEduFormService, SpecialtyEduFormService>();
 
-builder.Services.AddScoped<IBranchRepository , BranchRepository>();
-builder.Services.AddScoped<IBranchService , BranchService>();
+builder.Services.AddScoped<IDisciplineRepository, DisciplineRepository>();
+builder.Services.AddScoped<IDisciplineService, DisciplineService>();
 
+builder.Services.AddScoped<IFacultyRepository, FacultyRepository>();
+builder.Services.AddScoped<FacultyService, FacultyService>();
+
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
 
 var app = builder.Build();
 
